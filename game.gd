a22afa05722e13f3e5c7812b37cc8239bc6d0cb1
Maxwell Novak <novak.max@gmail.com>
2024-01-24 14:33:09 -0500

    Switch transition to be a side wipe instead of a fade

--- game.gd
+++ game.gd
@@ -4,27 +4,30 @@ const combatScene = preload("res://Events/Combat/Combat.tscn")
 const aquireCardScene = preload("res://Events/AquireCard/AquireCard.tscn")
 
 @onready
-var mapHolder = $Map
+var mapHolder = $Scenes/Map
 
 func _on_map_change_scene(type):
-	$AnimationPlayer.play("fade_out")
-	await $AnimationPlayer.animation_finished
+	var image = get_viewport().get_texture().get_image()
+	var texture = ImageTexture.create_from_image(image)
+	$Sprite2D.texture = texture
+	$Scenes.remove_child(mapHolder)
 	if type == LocationClass.Types.BATTLE:
 		var combat = combatScene.instantiate()
 		combat.combat_end.connect(_event_ended.bind(combat))
-		add_child(combat)
-		remove_child(mapHolder)
+		$Scenes.add_child(combat)
 	if type == LocationClass.Types.CARD:
 		var aquireCard = aquireCardScene.instantiate()
 		aquireCard.event_end.connect(_event_ended.bind(aquireCard))
-		add_child(aquireCard)
-		remove_child(mapHolder)
-	$AnimationPlayer.play("fade_in")
+		$Scenes.add_child(aquireCard)
+	$AnimationPlayer.play("side-wipe")
+	await $AnimationPlayer.animation_finished
 
 func _event_ended(event):
-	$AnimationPlayer.play("fade_out")
+	var image = get_viewport().get_texture().get_image()
+	var texture = ImageTexture.create_from_image(image)
+	$Sprite2D.texture = texture
+	$Scenes.remove_child(event)
+	$Scenes.add_child(mapHolder)
+	$AnimationPlayer.play("side-wipe")
 	await $AnimationPlayer.animation_finished
-	add_child(mapHolder)
-	remove_child(event)
 	event.queue_free()
-	$AnimationPlayer.play("fade_in")
