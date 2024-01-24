755f6f67e09a7f5dece66e8329603a99b8aafebf
Maxwell Novak <novak.max@gmail.com>
2024-01-24 16:58:51 -0500

    Fix enemy attack pointers and make cards display arrow

--- Card/card.gd
+++ Card/card.gd
@@ -22,6 +22,12 @@ var flippedCard: CardClass
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event.is_pressed():
 		card_clicked.emit(event.button_index)
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			$DamageContainer/DamageAmount.text = str(amount)
+			$NameContainer/CardName.text = cardName
+			if direction != Global.Direction.UNDEFINED:
+				$Direction.position.x = -$Direction.position.x
+				$Direction.rotate(deg_to_rad(180))
 
 func _on_area_2d_mouse_entered():
 	position.y = position.y - 30
@@ -31,8 +37,11 @@ func _on_area_2d_mouse_exited():
 	position.y = position.y + 30
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 
-func _process(_delta):
+func _ready():
 	$DamageContainer/DamageAmount.text = str(amount)
 	$NameContainer/CardName.text = cardName
-	if direction != 0:
-		$Direction.text = Global.Direction.keys()[direction]
+	if direction != Global.Direction.UNDEFINED:
+		$Direction.visible = true
+		if direction == Global.Direction.BACKWARDS:
+			$Direction.position.x = -$Direction.position.x
+			$Direction.rotate(deg_to_rad(180))
--- Events/Combat/GUI/battle_grid.gd
+++ Events/Combat/GUI/battle_grid.gd
@@ -3,9 +3,9 @@ extends Node2D
 func targetSpace(space: int):
 	var sprite = Sprite2D.new()
 	sprite.texture = load("res://assets/Asset 84@2x.png")
-	sprite.scale = Vector2(.1, .1)
+	sprite.scale = Vector2(0.01, 0.01)
 	sprite.rotate(deg_to_rad(90))
-	sprite.position.y = -550
+	sprite.position.y = -150
 	get_children()[space].add_child(sprite)
 
 func clearTargets():
