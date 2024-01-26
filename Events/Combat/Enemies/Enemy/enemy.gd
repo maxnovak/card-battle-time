8bef9b533c431dec3eb69d8c0f48401440a85298
Maxwell Novak <novak.max@gmail.com>
2024-01-26 18:51:34 -0500

    Add card showing enemies attack

--- Events/Combat/Enemies/Enemy/enemy.gd
+++ Events/Combat/Enemies/Enemy/enemy.gd
@@ -45,6 +45,8 @@ func chooseAction():
 		deck.append_array(discard)
 		discard.clear()
 	chosenAction = deck.pop_front()
+	$Card.amount = chosenAction.amount
+	$Card.cardName = chosenAction.cardName
 
 func playCard():
 	discard.append(chosenAction)
