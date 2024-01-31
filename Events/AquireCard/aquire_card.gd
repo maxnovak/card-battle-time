b616165c099751bacc96620d75309ae605f521cc
Maxwell Novak <novak.max@gmail.com>
2024-01-30 20:37:00 -0500

    Refactor aquire card to use control nodes and style better

--- Events/AquireCard/aquire_card.gd
+++ Events/AquireCard/aquire_card.gd
@@ -1,12 +1,12 @@
-extends Node2D
+extends Control
 
 signal event_end
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Card.card_clicked.connect(_on_card_clicked.bind($Card))
-	$Card2.card_clicked.connect(_on_card_clicked.bind($Card2))
-	$Card3.card_clicked.connect(_on_card_clicked.bind($Card3))
+	$GridContainer/Card.card_clicked.connect(_on_card_clicked.bind($GridContainer/Card))
+	$GridContainer/Card2.card_clicked.connect(_on_card_clicked.bind($GridContainer/Card2))
+	$GridContainer/Card3.card_clicked.connect(_on_card_clicked.bind($GridContainer/Card3))
 
 func _on_card_clicked(mouseButton, card):
 	if mouseButton == MOUSE_BUTTON_LEFT:
